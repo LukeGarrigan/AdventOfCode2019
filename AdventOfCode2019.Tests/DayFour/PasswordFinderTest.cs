c612fe4ff5e74b67045da4fe8ee743d94866c502
Luke Garrigan <lukegarrigan8@gmail.com>
2019-12-07 19:37:24 +0000

    Done day 4

---
+++ AdventOfCode2019.Tests/DayFour/PasswordFinderTest.cs
@@ -0,0 +1,72 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AdventOfCode2019.DayFour;
+using AdventOfCode2019.Helpers;
+
+namespace AdventOfCode2019.Tests
+{
+    [TestClass]
+    public class PasswordFinderTests
+    {
+
+        private PasswordFinder passwordFinder = new PasswordFinder();
+
+        [TestMethod]
+        public void TestAscending1()
+        {
+            Assert.IsTrue(passwordFinder.IsAscending(12));
+            Assert.IsTrue(passwordFinder.IsAscending(1234));
+            Assert.IsTrue(passwordFinder.IsAscending(122334499));
+        }
+
+
+        [TestMethod]
+        public void TestAscending2()
+        {
+            Assert.IsFalse(passwordFinder.IsAscending(212));
+            Assert.IsFalse(passwordFinder.IsAscending(223450));
+            Assert.IsTrue(passwordFinder.IsAscending(111111));
+        }
+
+
+        [TestMethod]
+        public void TestHasTwoAdjacent()
+        {
+            Assert.IsTrue(passwordFinder.HasTwoAdjacent(1122));
+            Assert.IsFalse(passwordFinder.HasTwoAdjacent(1234));
+        }
+
+
+
+        [TestMethod]
+        public void TestHasTwoAdjacentCont()
+        {
+            Assert.IsFalse(passwordFinder.HasTwoAdjacent(123789));
+        }
+
+        
+
+        [TestMethod]
+        public void TestPartOne()
+        {
+            Assert.AreEqual(1099, passwordFinder.CountCombinations(245182, 790572)); 
+        }
+
+
+        [TestMethod]
+        public void TestHasOnlyTwoAdjacent()
+        {
+            Assert.IsTrue(passwordFinder.HasOnlyTwoAdjacent(112222)); 
+            Assert.IsFalse(passwordFinder.HasOnlyTwoAdjacent(2313333)); 
+        }
+
+
+
+        [TestMethod]
+        public void TestPartTwo()
+        {
+            Assert.AreEqual(710, passwordFinder.CountCombinationsPartTwo(245182, 790572));
+        }
+
+    }
+}
---
+++ AdventOfCode2019/DayFour/PasswordFinder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using AdventOfCode2019.Helpers;
+using System.Collections.Generic;
+namespace AdventOfCode2019.DayFour
+{
+    public class PasswordFinder
+    {
+
+        public int CountCombinations(int start, int end)
+        {
+            var count = 0;
+            for (var i = start; i < end; i++)
+            {
+                if (IsAscending(i) && HasTwoAdjacent(i))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public bool IsAscending(int i)
+        {
+            var lastDigit = int.MaxValue;
+            while (i != 0)
+            {
+
+                if (lastDigit < i % 10)
+                {
+                    return false;
+                }
+
+                lastDigit = i % 10;
+                i = (int) Math.Floor(i / 10.0);
+            }
+
+            return true;
+        }
+
+
+        public int CountCombinationsPartTwo(int start, int end)
+        {
+            var count = 0;
+            for (var i = start; i < end; i++)
+            {
+                if (IsAscending(i) && HasOnlyTwoAdjacent(i))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
+        public bool HasTwoAdjacent(int i)
+        {
+            var lastDigit = int.MaxValue;
+            while (i != 0)
+            {
+
+                if (lastDigit == i % 10)
+                {
+                    return true;
+                }
+
+                lastDigit = i % 10;
+                i = (int)Math.Floor(i / 10.0);
+            }
+
+            return false;
+        }
+
+        public bool HasOnlyTwoAdjacent(int i)
+        {
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+
+            while (i != 0)
+            {
+                var lastDigit = i % 10;
+
+                if (counts.ContainsKey(lastDigit))
+                {
+                    counts[lastDigit] += 1;
+                }
+                else
+                {
+                    counts.Add(lastDigit, 1);
+                }
+                i = (int)Math.Floor(i / 10.0);
+            }
+
+            foreach (var x in counts)
+            {
+                if (x.Value == 2)
+                {
+                    return true;
+                }
+            
+            }
+            return false;
+        }
+    }
+}
